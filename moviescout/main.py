9ff11eaf240661e30b86d54e19b393269fe9ef7c
MuhammetAliVarlik <92933906+MuhammetAliVarlik@users.noreply.github.com>
2024-11-09 16:50:13 +0300

    The dataset has been reduced. The search algorithm has been integrated into the interface. The Semantic Search algorithm has begun.

--- moviescout/main.py
+++ moviescout/main.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import os
 
+base_dir = os.path.abspath(os.path.dirname(__file__))    
+df = pd.read_csv(os.path.join(base_dir, "../data/processed/TMDB.csv"))
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/search_movies', methods=['POST', 'GET'])
+@app.route('/search_movies', methods=['POST'])
 def search_movies():
-    movieName = request.form.get('movieName')  # Get the movie name from the form
-    return render_template('index.html', movieName=movieName)
+    movie_name = request.form.get('movieName', '')  # Get the movie name from the form
+    
+    # Filter the DataFrame to match the search query (case-insensitive)
+    filtered_df = df[df['title'].str.contains(movie_name, case=False, na=False)]
+    
+    # Create a list of dictionaries containing both 'title' and 'poster_url'
+    result = filtered_df[['title', 'poster_path']].to_dict(orient='records')
+    
+    # Return the list as a JSON response
+    return jsonify(result) # Converting to JSON for a proper response format
+
 if __name__ =="__main__":
     app.run(debug =True)
